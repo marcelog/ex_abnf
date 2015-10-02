986d1e9deba5a2e770b5165cda42ecfcc487f3e5
Marcelo Gornstein <marcelog@gmail.com>
2015-10-02 12:24:21 -0300

    allowing elixir 1.0.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ABNF.Mixfile do
      name: "ex_abnf",
      source_url: "https://github.com/marcelog/ex_abnf",
      version: "0.2.5",
-     elixir: "> 1.0.0",
+     elixir: ">= 1.0.0",
      description: description,
      package: package,
      deps: deps]
